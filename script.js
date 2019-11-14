573ef9d0f152500355fb64af01a03aab18045aff
AM0524 <54602716+AM0524@users.noreply.github.com>
2019-11-14 14:33:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function makeOpaque(el){
+	el.classList.add("opaque");
+}
+
+function resetAll(){
+document.location.reload("true");
+}
+
+function turnPurple(el){
+	document.getElementById("box1").classList.add("purple");
+
+}
+
+function changePicture(el) {
+	el.src = "image/transparent_shy.png";
+	document.getElementById("boo").classList.add("image/transparent_shy.PNG")
+		document.getElementById("boo").classList.remove("image/transparent_shy.PNG")
+	
+}
+
+
+// var a = ;
+//var b = ;
+//var c = document.getElementsByClassName("box 1") document.getElementsByClassName("box 2")document.getElementsByClassName("box 3")document.getElementsByClassName("box 4")document.getElementsByClassName("box 5") document.getElementsByClassName("box 6");
+
+//a = 1
+//b = 3
+//c = 6
+
+//function checkEquivalence() {
+	
+
